e5a20201456d9ab506a9c19afe0b7434a0646d8f
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2023-11-27 16:10:07 +0530

    Declaring variables and store data types
    Declaring variables and store String, float, Double

---
+++ Variable_intro.java
@@ -0,0 +1,18 @@
+public class Variable_intro {
+    public static void main (String args[]) {
+    //To declare a text variable
+    String name = "Bhimsen Joshi";
+
+    //To store number in variable
+    int a = 92;
+
+    //To store decimal in variable
+    double b = 20.1;
+
+    System.out.println(name);
+    System.out.println(a);
+    System.out.print(b);
+
+
+    }
+}
