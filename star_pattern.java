f0b71e2f0e0d3a10b3e5a5eea6eaab521df7edb1
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2023-11-27 15:41:11 +0530

    Print the star pattern using \n

---
+++ star_pattern.java
@@ -0,0 +1,11 @@
+//Print the star pattern
+//*
+//* *
+//* * * 
+//* * * *
+
+class star_pattern{
+    public static void main (String args[]){
+        System.out.print("*\n* *\n* * *\n* * * *");
+    }
+}
