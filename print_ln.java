b20b9e26050370ed3abc229efbc21d60ace6bee9
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2023-11-27 15:26:08 +0530

    print, println and \n methods to break line

--- print_ln.java
+++ print_ln.java
@@ -8,3 +8,4 @@ class println{
 //before running second time 
 //use println instead of print
 //for separate outputs in two different lines
+//can also use \n with text to perform ecxact same line break function
