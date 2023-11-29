c4bf2cbe56ee4bb297c749e5a35b7f92eb70d0c1
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2023-11-29 12:48:54 +0530

    area and peri of rect.

---
+++ areaperi.java
@@ -0,0 +1,8 @@
+public class areaperi {
+    public static void main(String[] args){
+        double width = 5.5;
+        double height = 8.5;
+        System.out.println("Area is = " + width*height);
+        System.out.println("Perimeter is = " + 2*width*height);
+    }
+}
