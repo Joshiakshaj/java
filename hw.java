7c93bc15dccf7fe90285c38891a564669c16c0cd
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2023-12-01 17:37:48 +0530

    basic loop implementation

---
+++ hw.java
@@ -0,0 +1,8 @@
+public class hw {
+    public static void main(String[] args) {
+                             //i++
+        for (int i = 0; i<100; i = i+1){
+            System.out.println(i);
+        }
+    }
+}
