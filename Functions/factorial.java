53c49131685e645ec47de4d77e3c1cdcc89c6311
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2023-12-07 21:19:12 +0530

    Print factorial of n

--- Functions/factorial.java
+++ Functions/factorial.java
@@ -4,6 +4,10 @@ public class factorial {
 
     public static void printFactorial(int n){
 
+        if(n<0){
+            System.out.println("Invalid Number");
+            return;
+        }
         int factorial = 1;
         for(int i =n; i>=1; i--){
             factorial = factorial*i;
